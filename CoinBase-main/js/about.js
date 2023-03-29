f6f8ab34619a0c4d01e32092d881b0942c7f05b0
Mohammed Attaallah <memo.1996.2000@gmail.com>
2023-03-29 21:55:56 +0200

    initial commit

---
+++ CoinBase-main/js/about.js
@@ -0,0 +1,29 @@
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '50px',
+    slidesToShow: 3,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 5
+            }
+    },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+    }
+  ]
+});
---
+++ CoinBase-main/js/main.js
@@ -0,0 +1,25 @@
+$('#dropdown-toggler').click(function (e) {
+    e.preventDefault();
+    $('.sell .dropdown').slideToggle()
+})
+$('.open-model').click(function () {
+    $('.model').fadeIn();
+
+})
+$('.model-close').click(function () {
+    $('.model').fadeOut();
+})
+$('.test').click(function () {
+    $('.mod').fadeIn();
+})
+
+$(".mod-close").click(function () {
+    $('.mod').fadeOut();
+})
+$('.test1').click(function () {
+    $('.mod1').fadeIn();
+})
+
+$(".mod-close").click(function () {
+    $('.mod1').fadeOut();
+})
